0438fe8f9489dd52ac7fe90b8c41f86b6e225686
Manon Bonnaud-Dubois <bonnaudduboism@gmail.com>
2024-11-18 17:30:50 +0100

    ✨ order et sort fonctionnent dans url

--- src/api/characters-api.js
+++ src/api/characters-api.js
@@ -2,13 +2,26 @@ import characters from '../data/characters.json';
 
 
 // Fonction qui retourne la liste des personnages triée
-export function getCharacters({ sortBy = 'name', sortDirection = 'asc' } = {}) {
+export function getCharacters({ sort = 'name', order = 'asc' } = {}) {
+    // console.log('sortBy', sort);
+    // console.log('sortDirection', order);
     return characters.slice().sort((a, b) => {
-        if (sortDirection === 'asc') {
-            return a[sortBy].localeCompare(b[sortBy]);
-        } else {
-            return b[sortBy].localeCompare(a[sortBy]);
+        if (sort === 'name') {
+            if (order === 'asc') {
+                return a.name.localeCompare(b.name);
+            } else {
+                return b.name.localeCompare(a.name);
+            }
+        } else if (sort === 'modified') {
+            const dateA = new Date(a.modified);
+            const dateB = new Date(b.modified);
+            if (order === 'asc') {
+                return dateA - dateB;
+            } else {
+                return dateB - dateA;
+            }
         }
+        return 0; // Default case if sort parameter is not recognized
     });
 }
 
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,4 +1,4 @@
-import { getCharacterById, getSortedCharacters } from "./api/characters-api";
+import { getCharacterById, getCharacters } from "./api/characters-api";
 import CharacterDetailPage from "./pages/CharacterDetailPage";
 import Layout from "./Layout";
 import AboutPage from "./pages/AboutPage";
@@ -17,7 +17,9 @@ const routes = [
                     const url = new URL(request.url);
                     const sort = url.searchParams.get('sort') || 'name';
                     const order = url.searchParams.get('order') || 'asc';
-                    return getSortedCharacters(sort, order);
+                    // console.log('sort', sort);
+                    // console.log('order', order);
+                    return getCharacters({ sort, order });
                 }
             },
             {
