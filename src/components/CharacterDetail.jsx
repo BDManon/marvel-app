56227d088900375cba69dac1310c3ee847a71879
Manon Bonnaud-Dubois <bonnaudduboism@gmail.com>
2024-12-02 17:21:00 +0100

    ✨EVALUATION - Add date-fns for date formatting in CharacterDetail and CharactersList components

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,4 +1,7 @@
+import { format } from 'date-fns';
+
 const CharacterDetail = ({ character ={} }) => {
+    const formattedDate = format(new Date(character.modified), 'MMM dd, yyyy');
     return (
         <>
             <h2>{character.name}</h2>
@@ -6,7 +9,7 @@ const CharacterDetail = ({ character ={} }) => {
             { character.thumbnail && <img src={`${character.thumbnail.path}/standard_large.${character.thumbnail.extension}`} alt={character.name} /> }
 
             <p>{character.description}</p>
-            <p>Date de modification : {character.modified}</p>
+            <p>Date de modification : <small><strong>{formattedDate}</strong></small></p>
             {/* Add more details here */}
         </>
     );
--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,14 +1,18 @@
 import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
 
 export function CharactersList({characters = []}) {
     return (
     <ul id="characters">
-        {characters.map((character) => (
-          <li key={character.id}> 
-            <Link to={`/characters/${character.id}`}> 
-              {character.name}
-            </Link>
-          </li>
-        ))}
-      </ul>)
+        {characters.map((character) => {
+            const formattedDate = format(new Date(character.modified), 'MMM dd, yyyy');
+            return (
+                <li key={character.id}> 
+                    <Link to={`/characters/${character.id}`}> 
+                        {character.name} (<small><strong>{formattedDate}</strong></small>)
+                    </Link>
+                </li>
+            );
+        })}
+    </ul>)
 }
